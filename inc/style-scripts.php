adaae608dc8fc1b856cd80e1a66b717acdac62d2
guisfons <guilhermesfonsecaa@gmail.com>
2024-09-26 13:28:08 -0300

    changing ajax order

--- inc/style-scripts.php
+++ inc/style-scripts.php
@@ -19,10 +19,10 @@ function themeFiles() {
     wp_register_script('jquery-mask', get_stylesheet_directory_uri() . '/assets/js/jquery.mask.min.js', array('jquery'), null, true);
     wp_enqueue_script('jquery-mask');
 
-    wp_localize_script('main-js', 'myAjax', array('ajaxurl' => admin_url('admin-ajax.php')));
-
     wp_register_script('main-js', get_stylesheet_directory_uri() . '/assets/js/main.js', array('jquery', 'jquery-mask'), null, true);
     wp_enqueue_script('main-js');
+
+    wp_localize_script('main-js', 'myAjax', array('ajaxurl' => admin_url('admin-ajax.php')));
 
     // enqueueTargetAssets(getTargetType());
 }
